7df9ffbf43327bc9c411ffe0246d8815e0ea84c7
AmitSharet <amit.sharet@gmail.com>
2021-11-20 22:04:45 +0200

    added get_all_friends()

--- api/api.py
+++ api/api.py
@@ -1,7 +1,19 @@
 from flask import Flask
 
 app = Flask(__name__)
-
+friends_graph={'adar':['amit', 'lia', 'daniel','yonatan', 'michal'], 'amit':['adar', 'hadar', 'ohad']}
 @app.route('/')
 def index():
     return {'username': 'Adar'}
+
+@app.route('/get-all-friends/<user>')
+def get_all_friends(user):
+    if user in friends_graph:
+        list_of_friends = friends_graph[user]
+        return list_of_friends[0] #TODO : return all the list and not just the 0 place
+    else: return {"{user} not in friends_graph : {friends_graph}"}
+     
+    
+
+# /api/hug
+# SEND NOTIFICATION FOR EVERYONE TO Hug
